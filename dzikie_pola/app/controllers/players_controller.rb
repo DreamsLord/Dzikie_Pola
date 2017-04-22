b1ae94924786f9530047bd38505b41071a0efdc3
DreamsLord <adamludian@gmail.com>
2017-04-22 12:11:16 +0200

    add routes

--- dzikie_pola/app/controllers/players_controller.rb
+++ dzikie_pola/app/controllers/players_controller.rb
@@ -34,21 +34,21 @@ class PlayersController < ApplicationController
 	def update
 		if @player.update(player_params)
 			if player.save
-			flash.now[:notice] = " zmodyfikowano gracza"
-			redirect_to player_path
-		else
-			flash.now[:error] = "nie udało sie zmodyfikować gracza"
-			render :edit
+				flash.now[:notice] = " zmodyfikowano gracza"
+				redirect_to player_path
+			else
+				flash.now[:error] = "nie udało sie zmodyfikować gracza"
+				render :edit
+			end
 		end
-	end
 
-	def destroy
-		flash[:notice] = "usunięto gracza ID: #{player.id} name: #{player.name}"
-		@player.destroy
-	end
+		def destroy
+			flash[:notice] = "usunięto gracza ID: #{player.id} name: #{player.name}"
+			@player.destroy
+		end
 
-	def player_params
-		params.require(:player).permit(:name, :surname, :type, :nation, :age, :weight, :faith, :faith_in_the_magic,:courage,:devotion,:amorous,:honour,:law_and_order,:wealth,:reputation, :strong, :ability, :sense, :wisdom, :fancy, :head, :fortune, :initiative, :hit, :stout, :sabre, :matchlock, :spear)
-	end
+		def player_params
+			params.require(:player).permit(:name, :surname, :type, :nation, :age, :weight, :faith, :faith_in_the_magic,:courage,:devotion,:amorous,:honour,:law_and_order,:wealth,:reputation, :strong, :ability, :sense, :wisdom, :fancy, :head, :fortune, :initiative, :hit, :stout, :sabre, :matchlock, :spear)
+		end
 
-end
+	end
