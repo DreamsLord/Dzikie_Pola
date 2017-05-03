d6f39682a14bb23a8780f5cb63f9322cc699c9f7
DreamsLord <adamludian@gmail.com>
2017-05-03 11:58:18 +0200

    add

--- dzikie_pola/app/controllers/fighters_controller.rb
+++ dzikie_pola/app/controllers/fighters_controller.rb
@@ -3,7 +3,11 @@ class FightersController < ApplicationController
 	protect_from_forgery with: :null_session
 
 	def choose_players_to_fight
-		@fighters = params[:player]
+		loadIdFighters = params[:player] 
+		@fighters = []
+		loadIdFighters.each do |x|
+			@fighters << Player.find(x.to_i)
+		end
 
 
 
